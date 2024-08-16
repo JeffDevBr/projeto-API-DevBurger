e6744f9f3f0cabfa283e3d32a068f4f9d346271a
Jefferson <jeff.dev.br@gmail.com>
2024-08-16 10:35:38 -0300

    add nodemon e importando e configurando rota na app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-//importa o módulo express.
+// Importa o módulo express.
 import express from 'express'
+// Importa as rotas definidas no arquivo routes.js.
+import routes from './routes.js'
 
-//A classe App é definida para encapsular a configuração do servidor Express
+// A classe App é definida para encapsular a configuração do servidor Express.
 class App {
-    //No construtor (constructor), ele inicializa uma instância do Express (this.app = express()).
+    // No construtor (constructor), ele inicializa uma instância do Express (this.app = express()).
     // Chama os métodos middlewares() e routes() para configurar os middlewares e as rotas da aplicação.
     constructor() {
         this.app = express()
@@ -12,12 +14,16 @@ class App {
     }
 
     middlewares() {
-        //O método middlewares() adiciona um middleware que permite ao servidor processar dados no formato JSON nas requisições.
+        // O método middlewares() adiciona um middleware que permite ao servidor processar dados no formato JSON nas requisições.
         this.app.use(express.json())
     }
 
-    //O método routes() está presente, mas ainda não foi implementado. É onde você definirá suas rotas futuramente.
-    routes() { }
+    // O método routes() configura as rotas da aplicação.
+    // Usa o módulo de rotas importado para definir o comportamento das rotas.
+    routes() {
+        this.app.use(routes)
+    }
 }
-// Exporta a instância da aplicação Express
+
+// Exporta a instância da aplicação Express.
 export default new App().app
